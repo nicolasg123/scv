6e3520768f6c250d858572304250c055beee3b19
Nicolas Grossi <nicolas.grossi@gmail.com>
2021-08-13 10:09:33 -0300

    working on requested testCases

--- src/  home.test.ts
+++ src/  home.test.ts
@@ -16,7 +16,11 @@ afterEach(async () => {
   await page.close();
 });
 
-it("Home page should have the correct title", async () => {
+it("TestCases", async () => {
     await page.goto("https://github.com");
     await page.click('body > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu.HeaderMenu--logged-out.position-fixed.top-0.right-0.bottom-0.height-fit.position-lg-relative.d-lg-flex.flex-justify-between.flex-items-center.flex-auto > div.d-lg-flex.flex-items-center.px-3.px-lg-0.text-center.text-lg-left > div.position-relative.mr-3.d-inline-block > a');
-    await page.fill('[name="login"]', "nicolas.grossi@gmail.com")});
+    await page.fill('[name="login"]', "nicolas.grossi@gmail.com");
+    await page.fill('[name="password"]', "nico140778NCG");
+    await page.click('text=New');
+  });
+    
